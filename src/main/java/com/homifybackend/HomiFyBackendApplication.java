d358aa93b5b83813156e5f7834a061e9fa921bf3
KhoiVo <vominhkhoi299@gmail.com>
2025-12-05 23:55:37 +0700

    Initial commit

---
+++ src/main/java/com/homifybackend/HomiFyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.homifybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomiFyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomiFyBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/homifybackend/HomiFyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homifybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomiFyBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

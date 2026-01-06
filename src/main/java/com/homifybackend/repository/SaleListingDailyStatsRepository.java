9ec620af113602305e13dc6fbf89157bd65bf351
Tuan Anh <tuanh250105@gmail.com>
2026-01-06 09:58:46 +0700

    test

--- src/main/java/com/homifybackend/repository/SaleListingDailyStatsRepository.java
+++ src/main/java/com/homifybackend/repository/SaleListingDailyStatsRepository.java
@@ -1,12 +1,13 @@
 package com.homifybackend.repository;
 
-import com.homifybackend.model.SaleListingDailyStats;
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.time.LocalDate;
-import java.util.List;
+import com.homifybackend.model.SaleListingDailyStats;
 
 public interface SaleListingDailyStatsRepository extends JpaRepository<SaleListingDailyStats, Long> {
     

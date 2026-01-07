6cd3e79bb77856ce5d4ab754a247ffd2dfb4038c
Pineapple <168957797+Kimoanh2002@users.noreply.github.com>
2026-01-07 15:43:19 +0700

    update agentlistingcontroller

--- src/main/java/com/homifybackend/controller/salelisting/AgentListingController.java
+++ src/main/java/com/homifybackend/controller/salelisting/AgentListingController.java
@@ -24,7 +24,7 @@ import com.homifybackend.service.salelisting.AgentListingService;
 import jakarta.validation.Valid;
 
 @RestController
-@RequestMapping("/api/agentlistings")
+@RequestMapping("/agentlistings")
 public class AgentListingController {
     
     @Autowired
